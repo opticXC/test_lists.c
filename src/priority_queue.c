6f9da71ed14041a4ff8d992050961f297dd7cf55
Aabish Malik <aabishmalik3337@gmail.com>
2024-06-13 20:22:02 +0530

    intermediate 1

--- src/priority_queue.c
+++ src/priority_queue.c
@@ -1,91 +1,91 @@
 #include "priority_queue.h"
 #include <stdlib.h>
 
-priority_queue_t *priority_queue_create(){
-	priority_queue_t *queue = (priority_queue_t *) malloc(sizeof(priority_queue_t));
-	if (queue != NULL){
+priority_queue_t *priority_queue_create() {
+	priority_queue_t *queue = (priority_queue_t *)malloc(sizeof(priority_queue_t));
+	if (queue != NULL) {
 		queue->head = queue->tail = NULL;
 	}
 	return queue;
 }
 
-void priority_queue_enqueqe(priority_queue_t *queue, int data, int priority){
-	if(queue == NULL) return;
-	priority_node_t *node =  (priority_node_t *) malloc(sizeof(priority_node_t));
-	if(node == NULL) return;
+void priority_queue_enqueqe(priority_queue_t *queue, int data, int priority) {
+	if (queue == NULL) return;
+	priority_node_t *node = (priority_node_t *)malloc(sizeof(priority_node_t));
+	if (node == NULL) return;
 
 	node->data = data;
 	node->priority = priority;
-	if(queue->head == NULL){
+	if (queue->head == NULL) {
 		queue->head = queue->tail = node;
-	}else{
+	} else {
 		queue->tail->next = node;
 		queue->tail = node;
 	}
 }
 
-priority_node_t *priority_queue_dequeue(priority_queue_t *queue){
-	if (queue == NULL || queue->head == NULL) return  NULL;
+priority_node_t *priority_queue_dequeue(priority_queue_t *queue) {
+	if (queue == NULL || queue->head == NULL) return NULL;
 
 	priority_node_t *prev = NULL;
 	priority_node_t *max = queue->head;
 	priority_node_t *current = queue->head;
 	while (current->next != NULL) {
-		if( current->next->priority > max->priority ){
+		if (current->next->priority > max->priority) {
 			prev = current;
 			max = current->next;
 		}
 		current = current->next;
 	}
-	if(max == queue->head){
+	if (max == queue->head) {
 		queue->head = max->next;
 		return max;
 	}
-	if(max == queue->tail){
+	if (max == queue->tail) {
 		queue->tail = prev;
 	}
 	prev->next = max->next;
-	return  max;
+	return max;
 }
 
-priority_node_t *priority_queue_peek(priority_queue_t *queue){
-	if(queue == NULL || queue->head == NULL) return NULL;
+priority_node_t *priority_queue_peek(priority_queue_t *queue) {
+	if (queue == NULL || queue->head == NULL) return NULL;
 	priority_node_t *max = queue->head;
 	priority_node_t *current = max->next;
-	while(current != NULL){
-		if(current->priority > max->priority) max = current;
+	while (current != NULL) {
+		if (current->priority > max->priority) max = current;
 		current = current->next;
 	}
 	return max;
 }
 
-priority_node_t *priority_queue_search(priority_queue_t *queue, int data){
-	if(queue == NULL) return  NULL;
+priority_node_t *priority_queue_search(priority_queue_t *queue, int data) {
+	if (queue == NULL) return NULL;
 
 	priority_node_t *current = queue->head;
 
-	while(current != NULL){
-		if(current->data == data) return current;
+	while (current != NULL) {
+		if (current->data == data) return current;
 		current = current->next;
 	}
 
-	return  NULL;
+	return NULL;
 }
 
-size_t priority_queue_length(priority_queue_t *queue){
-	if(queue == NULL || queue->head == NULL) return 0;
+size_t priority_queue_length(priority_queue_t *queue) {
+	if (queue == NULL || queue->head == NULL) return 0;
 	int count = 0;
 	priority_node_t *current = queue->head;
-	while(current != NULL){
+	while (current != NULL) {
 		count++;
-		current =current->next;
+		current = current->next;
 	}
 	return count;
 }
 
-void priority_queue_destroy(priority_queue_t *queue){
+void priority_queue_destroy(priority_queue_t *queue) {
 	if (queue == NULL) return;
-	
+
 	priority_node_t *current = priority_queue_dequeue(queue);
 	while (current != NULL) {
 		free(current);
